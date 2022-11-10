aef34b916bb7bcb7e538c2516a0e6e480506e840
Tunglies <Tunglies@outlook.com>
2022-11-10 19:29:12 +0800

    without number address

--- script_fetch_domain.py
+++ script_fetch_domain.py
@@ -7,6 +7,7 @@ with open("./trackers.txt") as tf:
 
 domains = re.findall("\/\/(.*)(?=:)", trackers)
 deduplicate_domains = set(domains)
+without_number_address = list(filter(lambda x: not all(map(lambda y: y.isdigit(), x.split("."))), deduplicate_domains))
 
 with open("./domains.txt", "w+") as df:
-    df.write("\n".join(deduplicate_domains))
+    df.write("\n".join(without_number_address))
